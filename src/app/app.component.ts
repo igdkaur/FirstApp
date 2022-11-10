08d8d1064f13ac01f02839ed6412af49eeb36c39
GDK <igdkaur@gmail.com>
2022-11-10 17:52:00 +0530

    q3 angular app load and start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root', // handles app-root in index.html
+  selector: 'app-root', //handles app-root in index.html
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
