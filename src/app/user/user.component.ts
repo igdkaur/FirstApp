d258e6b96b80489ad419b9e8cf269c8f99cb2eb2
GDK <igdkaur@gmail.com>
2022-11-10 18:59:09 +0530

    q4.1 - create custom component

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+
+
+@Component ( {
+  selector:'app-user',
+  templateUrl:'./user.component.html'
+})
+export class UserComponent{
+  
+}
+
+
+// pass meta data to the decorator 
+// tells what to do with the class
+// data is passed as js obj
