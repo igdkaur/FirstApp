7c9c92e32fbc31a1331120589b7121f92ebca764
GDK <igdkaur@gmail.com>
2022-11-11 10:31:50 +0530

    q7.2 inline styles using styles property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root', //handles app-root in index.html
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
+  styles: [`
+  h3{
+    color: blue;
+  }
+  `]
 })
 export class AppComponent {
  
 }
+
+
+// style URLs is an array because we could reference multiple external style sheets here.
+// could add pointers to other style sheets.
+
+// can also set up another property,styles.
+// takes an array of strings, define the style in this string.
+// use backticks again to be able to write a multi-line expression here.
