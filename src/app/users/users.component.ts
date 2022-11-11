9c2c5aa47beccb05c306822f5099a542976ecd0b
GDK <igdkaur@gmail.com>
2022-11-11 11:25:36 +0530

    q8.1 change the selector to be an attribute

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-users',
+  selector: '[app-users]',
   // templateUrl: './users.component.html',
   template: '<app-user></app-user> ',
   styleUrls: ['./users.component.css']
